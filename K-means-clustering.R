053098f392f80d2403cd8764181ba7ede7e4c129
Torbet McNeil <56936008+tmcneil-g@users.noreply.github.com>
2022-11-01 23:11:26 -0700

    Update K-means-clustering.R

--- K-means-clustering.R
+++ K-means-clustering.R
@@ -7,21 +7,30 @@
 # Using k-means clustering, where x-axis is the institution and y-axis is a 
 #specific area/sub-area in the NASEM framework
 
-#read the dataset
-scores.programs <- read.csv(file = "scores-nasem.csv")
+install.packages(factoextra)
+library(factoextra)
 
+#read the dataset
+programs.scores <- read.csv(file = "scores-nasem.csv")
+
+# Drop any NA rows
+#programs.scores <- na.omit(programs.scores)
 
 #select a subset of the variables
-bdiag.2vars <- bdiag.data[,c("Institution", "NASEM.Domain.01")]
+programs.domain <- programs.scores[,c("Institution", "NASEM.Domain.01")]
 
-#compute the # clusters chosen 
+#compute the number of chosen clusters 
+km <- kmeans(programs.domain, centers = 3)
+km
+
+fviz_cluster(km, data = programs.domain, label=NA)+theme_bw()
 
 
 
+#parts of code from An Introduction to Statistical Learning book that I am not using now
 #set.seed (4)
-
-km.out <- kmeans (x, 3, centers = 3)
-kmeans()
-par (mfrow = c(1, 2))
-plot (x, col = (km.out$cluster + 1),
-main = "K- Means Clustering Results with K = 2", xlab = "Program", ylab = "Score", pch = 20, cex = 2)
+#km.out <- kmeans (x, 3, centers = 3)
+#kmeans()
+#par (mfrow = c(1, 2))
+#plot (x, col = (km.out$cluster + 1),
+#main = "K- Means Clustering Results with K = 2", xlab = "Program", ylab = "Score", pch = 20, cex = 2)
