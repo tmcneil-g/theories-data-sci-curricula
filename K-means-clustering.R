dcd072dc37c158ee93e5fc55598908e334c865c5
Torbet McNeil <56936008+tmcneil-g@users.noreply.github.com>
2022-11-01 22:45:02 -0700

    Update K-means-clustering.R

--- K-means-clustering.R
+++ K-means-clustering.R
@@ -4,11 +4,23 @@
 # 2022-11-01
 
 #########################################################################
-# Using k-means clustering, where x-axis is the program and y-axis is a 
+# Using k-means clustering, where x-axis is the institution and y-axis is a 
 #specific area/sub-area in the NASEM framework
 
-set.seed (4)
-km.out <- kmeans (x, 3, nstart =    )
+#read the dataset
+scores.programs <- read.csv(file = "scores-nasem.csv")
+
+
+#select a subset of the variables
+bdiag.2vars <- bdiag.data[,c("Institution", "NASEM.Domain.01")]
+
+#compute the # clusters chosen 
+
+
+
+#set.seed (4)
+
+km.out <- kmeans (x, 3, centers = 3)
 kmeans()
 par (mfrow = c(1, 2))
 plot (x, col = (km.out$cluster + 1),
