173fae2a6b41d6fcf55cb8add92649051058d5f2
Torbet McNeil <56936008+tmcneil-g@users.noreply.github.com>
2022-11-02 22:30:26 -0700

    Update K-means-clustering.R

--- K-means-clustering.R
+++ K-means-clustering.R
@@ -4,8 +4,7 @@
 # 2022-11-01
 
 #########################################################################
-# Using k-means clustering, where x-axis is a specific area/sub-area and y-axis is a 
-# specific area/sub-area in the NASEM framework
+# Using k-means clustering
 # Using code from this website: https://bookdown.org/tpinto_home/Unsupervised-learning/k-means-clustering.html
 
 install.packages("factoextra")
@@ -20,6 +19,7 @@ programs.scores <- read.csv(file = "scores-nasem.csv")
 head(programs.scores)
 
 # select a subset of the variables
+# Here, the x-axis and y-axis are specific areas/sub-areas in the NASEM framework
 ethics.domain <- programs.scores[,c("NASEM.Ethics.01", "NASEM.Domain.01")]
 
 # compute the number of chosen clusters 
@@ -29,12 +29,23 @@ km
 # plot the results of the clusters in a scatter plot formed by the two variables
 fviz_cluster(km, data = ethics.domain, label=NA)+theme_bw()
 
+# select a subset of the variables
+# Here, the x-axis is the program, and the y-axis is a specific areas/sub-areas in the NASEM framework.
+# I re-coded the program name to a numerical number from 1-18
+program.domain <- programs.scores[,c("Institution.01", "NASEM.Domain.01")]
+
+# compute the number of chosen clusters 
+km <- kmeans(program.domain, centers = 3)
+km
+
+# plot the results of the clusters in a scatter plot formed by the two variables
+fviz_cluster(km, data = program.domain, label=NA)+theme_bw()
 
 
-#parts of code from An Introduction to Statistical Learning book that I am not using now
-#set.seed (4)
-#km.out <- kmeans (x, 3, centers = 3)
-#kmeans()
-#par (mfrow = c(1, 2))
-#plot (x, col = (km.out$cluster + 1),
-#main = "K- Means Clustering Results with K = 2", xlab = "Program", ylab = "Score", pch = 20, cex = 2)
+# parts of code from An Introduction to Statistical Learning book that I am not using now
+# set.seed (4)
+# km.out <- kmeans (x, 3, centers = 3)
+# kmeans()
+# par (mfrow = c(1, 2))
+# plot (x, col = (km.out$cluster + 1),
+# main = "K- Means Clustering Results with K = 2", xlab = "Program", ylab = "Score", pch = 20, cex = 2)
